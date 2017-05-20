9a88566e9d034a1c3d04a7459356051e929dc23f
Centro Ático <centroatico@202C11E25168966.local>
2017-05-20 13:26:31 -0500

    secion 7

--- instagram/instagramapp/urls.py
+++ instagram/instagramapp/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns=[
   url(r'^$', views.registro, name = 'registro'),
+   url(r'^$', views.registro, name = 'ingresar'),
+  url(r'crear_usuario/$', views.crer_usuario, name= 'crear_usuario'),
+
 ]
+s
--- instagram/instagramapp/views.py
+++ instagram/instagramapp/views.py
@@ -5,5 +5,21 @@ from django.shortcuts import render
 def registro (request):
     return render (request,'registro.html')
 
+def crear_usuario (request):
+
+    Nombre completo = request.POST['Nombre completo']
+    Correo electronico = request.POST['Correo electrónico']
+    Usuario = request.POST['Usuario']
+    Contraseña = request.POST['Contraseña']
+    Confirmar contraseña = request.POST['Confirmar contraseña']
+
+
+
+     print (Nombre completo)
+     print (Correo electrónico)
+     print (Usuario)
+     print (Contraseña)
+     print (Confirmar contraseña)
+
 def registro (request):
     return render (request,'ingresar.html')
